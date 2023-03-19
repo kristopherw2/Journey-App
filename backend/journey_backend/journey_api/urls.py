c561c3ab8dd6989928ed9479e43fdc2043680daa
ccanoniz <ccanoniz@yahoo.com>
2023-03-18 21:36:36 -0700

    Added API to add comment, nested post-comment, sorted post list

--- backend/journey_backend/journey_api/urls.py
+++ backend/journey_backend/journey_api/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.JourneyView.as_view(), name='journey_list'), # for listing and creating
-    # path('<int:pk>', views.JourneyView.as_view(), name='journey_detail'), # for detail, updating, and deleting
 
     #Routes related to Post 
-    path('post', views.JourneyView.as_view(), name='journey_list'), 
-    path('post/<int:pk>', views.JourneyView.as_view(), name='journey_detail'), 
+    path('post', views.PostView.as_view(), name='post_list'), 
+    path('post/<int:post_pk>', views.PostView.as_view(), name='post_detail'), 
 
     #Routes related to Comment
-    path('comment', views.CommentView.as_view(), name='comment_list'), 
-    path('comment/<int:comment_pk>', views.CommentView.as_view(), name='comment_detail'), 
+    path('post/<int:post_pk>/comments', views.CommentView.as_view(), name='comment_list'), 
+    path('post/<int:post_pk>/comments/<int:comment_pk>', views.CommentView.as_view(), name='comment_details'), 
+
+
+ 
 
 ]
 
--- backend/journey_backend/journey_api/views.py
+++ backend/journey_backend/journey_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
-from .serializers import JourneySerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer
 from rest_framework.pagination import PageNumberPagination
 from .models import PostDB, CommentDB
 
@@ -12,10 +12,9 @@ from .models import PostDB, CommentDB
 class BasicPagination(PageNumberPagination):
     page_size_query_param = 'limit'
 
-class JourneyView(APIView):
+class PostView(APIView):
 
     pagination_class = BasicPagination
-    # serializer_class = JourneySerializer
 
     @property
     def paginator(self):
@@ -39,62 +38,59 @@ class JourneyView(APIView):
         assert self.paginator is not None
         return self.paginator.get_paginated_response(data)
 
-    def get(self, request, pk=None):
-        if pk:  
-            print("#########################")
-            data = PostDB.objects.get(pk=pk)
-            print("#########################")
-            serializer = JourneySerializer(data)
+    def get(self, request, post_pk=None):
+        if post_pk:  
+            data = PostDB.objects.get(pk=post_pk)
+            serializer = PostSerializer(data)
         else:
-            data = PostDB.objects.all()
+            data = PostDB.objects.all().order_by('-date_posted')
             page = self.paginate_queryset(data)
             if page is not None:
-                serializer = self.get_paginated_response(JourneySerializer(page,
+                serializer = self.get_paginated_response(PostSerializer(page,
                 many=True).data)
             else:
-                serializer = JourneySerializer(data, many=True)
+                serializer = PostSerializer(data, many=True)
         return Response({"result": serializer.data})
 
     def post(self, request):
         hiking_post = request.data
-        serializer = JourneySerializer(data=hiking_post)
+        serializer = PostSerializer(data=hiking_post)
         if serializer.is_valid(raise_exception=True):
             hiking_post_saved = serializer.save()
         return Response({"result": f"SUCCESSFULLY ADDED - New Hiking Post: {hiking_post_saved.description}"})
 
-    def put(self, request, pk):
-        saved_hiking_post = get_object_or_404(PostDB.objects.all(), pk=pk)
+    def put(self, request, post_pk):
+        saved_hiking_post = get_object_or_404(PostDB.objects.all(), pk=post_pk)
         data = request.data
-        serializer = JourneySerializer(instance=saved_hiking_post, data=data, partial=True) #partial means not all fields are required 
-        #The .is_valid() method takes an optional raise_exception flag that will cause it to raise a serializers.ValidationError exception if there are validation errors.
+        serializer = PostSerializer(instance=saved_hiking_post, data=data, partial=True) 
         if serializer.is_valid(raise_exception=True):#
             saved_hiking_post = serializer.save()
-        return Response({"result": f"SUCCESSFULLY UPDATED - {saved_hiking_post.id} updated"})
+        return Response({"result": f"SUCCESSFULLY UPDATED - Post id: {saved_hiking_post.id} "})
 
-    def delete(self, request, pk):
-        hiking_post = get_object_or_404(PostDB.objects.all(), pk=pk)
+    def delete(self, request, post_pk):
+        hiking_post = get_object_or_404(PostDB.objects.all(), pk=post_pk)
         hiking_post.delete()
-        return Response({"result": f"SUCCESSFULLY DELETED - Post id {pk} deleted"},status=204)
+        return Response({"result": f"SUCCESSFULLY DELETED - Post id: {post_pk} deleted"},status=204)
 
 class CommentView(APIView):
 
-    def get(self, request, comment_pk=None):
+    def get(self, request, post_pk, comment_pk=None):
         if comment_pk:  
             data = CommentDB.objects.get(pk=comment_pk)
             serializer = CommentSerializer(data)
         else:
-            data = CommentDB.objects.all()
+            data = CommentDB.objects.filter(post_id=post_pk)
             serializer = CommentSerializer(data, many=True)
         return Response({"result": serializer.data})
 
-    def post(self, request):
+    def post(self, request, post_pk):
         comment = request.data
         serializer = CommentSerializer(data=comment)
         if serializer.is_valid(raise_exception=True):
             comment_saved = serializer.save()
         return Response({"result": f"SUCCESSFULLY ADDED - Comment: {comment_saved.comment_text}"})
 
-    def put(self, request, comment_pk):
+    def put(self, request, post_pk, comment_pk):
         saved_comment = get_object_or_404(CommentDB.objects.all(), pk=comment_pk)
         data = request.data
         serializer = CommentSerializer(instance=saved_comment, data=data, partial=True) 
@@ -102,7 +98,7 @@ class CommentView(APIView):
             saved_comment = serializer.save()
         return Response({"result": f"SUCCESSFULLY UPDATED - Comment id : {saved_comment.id}"})
 
-    def delete(self, request, comment_pk):
+    def delete(self, request, comment_pk, post_pk):
         comment = get_object_or_404(CommentDB.objects.all(), pk=comment_pk)
         comment.delete()
         return Response({"result": f"SUCCESSFULLY DELETED - Comment id {comment_pk} deleted"},status=204)
