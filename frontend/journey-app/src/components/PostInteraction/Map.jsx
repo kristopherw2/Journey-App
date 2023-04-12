52405fd35d72aad9d8749dee81e9d8cd3c65d70c
tsmith4014 <chjthomps@gmail.com>
2023-04-11 20:55:29 -0400

    fixed map rendering narrow/removed "Map IT"

--- frontend/journey-app/src/components/PostInteraction/Map.jsx
+++ frontend/journey-app/src/components/PostInteraction/Map.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
+import "./Map.css"; // Import the new CSS file
 import L from "leaflet";
 
 const geotagIcon = new L.Icon({
@@ -15,7 +16,7 @@ const Map = ({ lat, lng }) => {
   const position = [lat, lng];
 
   return (
-    <div style={{ height: "300px", width: "100%" }}>
+    <div className="map-container" style={{ height: "300px", width: "100%" }}>
       <MapContainer center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -32,3 +33,40 @@ const Map = ({ lat, lng }) => {
 };
 
 export default Map;
+
+
+
+// import React from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+// import "leaflet/dist/leaflet.css";
+// import L from "leaflet";
+
+// const geotagIcon = new L.Icon({
+//   iconUrl: "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png",
+//   iconSize: [32, 32],
+//   iconAnchor: [16, 32],
+//   popupAnchor: [0, -32],
+// });
+
+// const Map = ({ lat, lng }) => {
+//   const position = [lat, lng];
+
+//   return (
+//     <div style={{ height: "300px", width: "100%" }}>
+//       <MapContainer center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
+//         <TileLayer
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         <Marker position={position} icon={geotagIcon}>
+//           <Popup>
+//             Latitude: {lat}, Longitude: {lng}
+//           </Popup>
+//         </Marker>
+//       </MapContainer>
+//     </div>
+//   );
+// };
+
+// export default Map;
